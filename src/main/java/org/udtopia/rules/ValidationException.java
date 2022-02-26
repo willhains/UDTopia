b45335acd87c3eb6d704cf55775b95c9e4380266
Will Hains <willhains@udtopia.org>
2022-02-26 12:31:55 +0900

    #3: Add `ValidationException` constructor for strings.
    Add another constructor that shortens long input values, for safety.
    It keeps the start and end of the string, for easier troubleshooting.
    It replaces the middle of the string with `...`.

--- src/main/java/org/udtopia/rules/ValidationException.java
+++ src/main/java/org/udtopia/rules/ValidationException.java
@@ -15,4 +15,29 @@ public class ValidationException extends RuntimeException
 	{
 		super(target.getSimpleName() + ": " + failureReason);
 	}
+
+	/**
+	 * Use this constructor when the value in error could be an unbounded string.
+	 *
+	 * @param target the class declaring the violated rule.
+	 * @param failureReason a helpful description of the error.
+	 * @param valueInError a string representation of the value that caused this exception.
+	 * 	For safety, this constructor will shorten it to a maximum length of {@value #VALUE_LENGTH_THRESHOLD} in the
+	 * 	exception message.
+	 */
+	public ValidationException(final Class<?> target, final String failureReason, final String valueInError)
+	{
+		this(target, failureReason + ": " + _abbreviateString(valueInError));
+	}
+
+	static final int VALUE_LENGTH_THRESHOLD = 256;
+	private static final int _PREFIX_LENGTH = VALUE_LENGTH_THRESHOLD / 2 - 1;
+	private static final int _SUFFIX_LENGTH = _PREFIX_LENGTH - 1;
+
+	private static String _abbreviateString(final String valueInError)
+	{
+		final int length = valueInError.length();
+		if (length <= VALUE_LENGTH_THRESHOLD) { return valueInError; }
+		return valueInError.substring(0, _PREFIX_LENGTH) + "..." + valueInError.substring(length - _SUFFIX_LENGTH);
+	}
 }
--- src/test/java/org/udtopia/rules/ValidationExceptionTest.java
+++ src/test/java/org/udtopia/rules/ValidationExceptionTest.java
@@ -1,16 +1,57 @@
 package org.udtopia.rules;
 
+import java.util.Arrays;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
+import static org.udtopia.rules.ValidationException.*;
 
 public class ValidationExceptionTest
 {
+	private String _almostTooLongString;
+
+	@Before public void prepareLongString()
+	{
+		final char[] chars = new char[VALUE_LENGTH_THRESHOLD - 4];
+		Arrays.fill(chars, 'X');
+		_almostTooLongString = "abc" + new String(chars); // "abcXXX~XXX" (length = 255)
+	}
+
 	@Test public void shouldIncludeClassNameInExceptionMessage()
 	{
 		class X { }
-		final ValidationException x = new ValidationException(X.class, "test");
-		assertThat(x.getMessage(), is("X: test"));
+		final ValidationException x = new ValidationException(X.class, "reason");
+		assertThat(x.getMessage(), is("X: reason"));
+		final ValidationException y = new ValidationException(X.class, "reason", "value");
+		assertThat(y.getMessage(), is("X: reason: value"));
+	}
+
+	@Test public void shouldNotAbbreviateStringWhenLessThanThreshold()
+	{
+		class X { }
+		final ValidationException x = new ValidationException(X.class, "reason", _almostTooLongString);
+		final String message = x.getMessage();
+		assertThat(message, is("X: reason: " + _almostTooLongString));
+	}
+
+	@Test public void shouldNotAbbreviateStringWhenEqualToThreshold()
+	{
+		class X { }
+		final ValidationException x = new ValidationException(X.class, "reason", _almostTooLongString + "O");
+		final String message = x.getMessage();
+		assertThat(message, is("X: reason: " + _almostTooLongString + "O"));
+	}
+
+	@Test public void shouldAbbreviateStringWhenBiggerThanThreshold()
+	{
+		class X { }
+		final ValidationException x = new ValidationException(X.class, "reason", _almostTooLongString + "OO");
+		final String message = x.getMessage();
+		assertThat(message.length(), is("X: reason: ".length() + VALUE_LENGTH_THRESHOLD));
+		assertThat(message, startsWith("X: reason: " + "abcXXX"));
+		assertThat(message, containsString("..."));
+		assertThat(message, endsWith("XXXOO"));
 	}
 }
