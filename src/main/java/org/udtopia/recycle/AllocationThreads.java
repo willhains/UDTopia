34eddb743a8f4f12878f2f6bd1902444943ae46c
Will Hains <git@willhains.com>
2022-11-25 09:25:12 +0000

    alpha: Move annotations in enum to appease newer compiler.

--- src/main/java/org/udtopia/recycle/AllocationThreads.java
+++ src/main/java/org/udtopia/recycle/AllocationThreads.java
@@ -2,24 +2,23 @@ package org.udtopia.recycle;
 
 import java.util.function.Function;
 import org.udtopia.ThreadSafe;
-import org.udtopia.Value;
 
 import static java.lang.ThreadLocal.*;
 
 /**
  * Tuning parameter for how a {@link Recyclable} class will be allocated.
  */
-public enum AllocationThreads
+public @ThreadSafe enum AllocationThreads
 {
 	/** Only one thread will ever call {@link RecycleBin#recycle}. This gives the best performance. */
-	@Value SINGLE_THREADED(RingBufferRecycleBin::new),
+	SINGLE_THREADED(RingBufferRecycleBin::new),
 
 	/**
 	 * Multiple threads may call {@link RecycleBin#recycle}. This is the safest (and default) option.
 	 * Each thread gets its own pool of objects, so the memory usage increases with thread count.
 	 * Latency is slightly slower than {@link #SINGLE_THREADED}.
 	 */
-	@ThreadSafe THREAD_LOCAL(binSize ->
+	THREAD_LOCAL(binSize ->
 	{
 		final ThreadLocal<RecycleBin<Recyclable>> threadLocal = withInitial(() -> new RingBufferRecycleBin<>(binSize));
 		return (recycler, filler) -> threadLocal.get().recycle(recycler, filler);
